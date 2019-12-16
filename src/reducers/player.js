76a0eef27a7e8f50a7bb32691be0c279cbf8edc7
Andy Shih <a.shih@coldwhere.com>
2019-12-16 17:32:37 -0600

    Initial commit

---
+++ src/reducers/player.js
@@ -0,0 +1,87 @@
+import * as PlayerActionTypes from '../actiontypes/player';
+
+const initialState = {
+	players: [
+    {
+      name: 'Jim Hoskins',
+      score: 31,
+      created: '11/8/2016',
+      updated: '11/9/2016'
+    },
+    {
+      name: 'Andrew Chalkley',
+      score: 20,
+      created: '11/9/2016',
+      updated: '11/10/2016'
+    },
+    {
+      name: 'Alena Holligan',
+      score: 50,
+      created: '11/11/2016',
+      updated: '11/12/2016'
+    }
+	],
+	selectedPlayerIndex: -1
+}
+
+// Must implement a pure function that does not mutate state.
+export default function Player(state = initialState, action) {
+
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  switch (action.type) {
+
+    case PlayerActionTypes.ADD_PLAYER:
+      const addPlayerList = [
+        ...state.players,
+        {
+          name: action.name,
+          score: 0,
+          created: `${month}/${day}/${year}`
+        }
+      ];
+      return {
+        ...state,
+        players: addPlayerList
+      };
+    
+    case PlayerActionTypes.REMOVE_PLAYER:
+      const removePlayerList = [
+        ...state.players.slice(0, action.index),
+        ...state.players.slice(action.index + 1)
+      ];
+      return {
+        // ...state,
+        players: removePlayerList,
+        selectedPlayerIndex: (action.index === state.selectedPlayerIndex) ? -1 : state.selectedPlayerIndex
+      }
+    
+    case PlayerActionTypes.UPDATE_PLAYER_SCORE:
+      const updatePlayerList = state.players.map((player, index) => {
+        if (index === action.index) {
+          return {
+            ...player,
+            score: player.score + action.score,
+            updated: `${month}/${day}/${year}`
+          };
+        }
+        return player;
+      });
+      return {
+        ...state,
+        players: updatePlayerList
+      };
+
+    case PlayerActionTypes.SELECT_PLAYER:
+      return {
+        ...state,
+        selectedPlayerIndex: action.index
+      };
+
+    default:
+      return state;
+  }
+}
